ddc4fe98e790ef37df632c9bac374cd8da30b2f6
santoIN <nfssnt03d03d086j@studenti.unical.it>
2024-11-18 16:57:42 +0100

    Delete esercitazione11112024 directory

--- esercitazione11112024/dao/src/CalciatoreDao.java
+++
@@ -1,9 +0,0 @@
-import java.util.List;
-
-public interface CalciatoreDao {
-    void createCalciatore(Calciatore calciatore);
-    Calciatore getCalciatoreById(int id);
-    List<Calciatore> getAllCalciatori();
-    void updateCalciatore(Calciatore calciatore);
-    void deleteCalciatore(int id);
-}
--- esercitazione11112024/dao/src/calciatori.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE calciatori (
-                            id INT AUTO_INCREMENT PRIMARY KEY,
-                            nome VARCHAR(100),
-                            squadra VARCHAR(100),
-                            gol INT
-);
